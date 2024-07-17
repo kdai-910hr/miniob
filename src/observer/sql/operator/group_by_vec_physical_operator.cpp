024c5aedcc7edcbd096dac5db211f6ba5f7d9836
z <zyf79513@163.com>
2024-07-17 16:04:11 +0800

    1604

--- src/observer/sql/operator/group_by_vec_physical_operator.cpp
+++ src/observer/sql/operator/group_by_vec_physical_operator.cpp
@@ -8,27 +8,94 @@ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
+#include <algorithm>
 #include "sql/operator/group_by_vec_physical_operator.h"
+#include "sql/expr/aggregate_state.h"
 
-RC GroupByVecPhysicalOperator::open(Trx *trx) {
-  // ASSERT(children_.size() == 1, "group by operator only support one child, but got %d", children_.size());
+GroupByVecPhysicalOperator::GroupByVecPhysicalOperator(
+  std::vector<std::unique_ptr<Expression>> &&group_by_exprs, std::vector<Expression *> &&expressions): hash_table_(expressions), scanner_(&hash_table_) {
+  group_by_exprs_ = std::move(group_by_exprs);
+  aggregate_expressions_ = std::move(expressions);
+  value_expressions_.reserve(aggregate_expressions_.size());
+  std::ranges::for_each(aggregate_expressions_, [this](Expression *expr) {
+    auto *aggregate_expr = static_cast<AggregateExpr *>(expr);
+    Expression *child_expr = aggregate_expr->child().get();
+    ASSERT(child_expr != nullptr, "aggregate expression must have a child expression");
+    value_expressions_.emplace_back(child_expr);
+  });
+  auto group_size = group_by_exprs_.size();
+  for (size_t i = 0; i < group_size; i++) {
+    auto expr = group_by_exprs_[i].get();
+    switch (expr->value_type()) {
+      case AttrType::INTS:
+        output_chunk_.add_column(make_unique<Column>(AttrType::INTS, sizeof(int)), i);
+        break;
+      case AttrType::FLOATS:
+        output_chunk_.add_column(make_unique<Column>(AttrType::FLOATS, sizeof(float)), i);
+        break;
+      case AttrType::CHARS:
+        output_chunk_.add_column(make_unique<Column>(AttrType::CHARS, expr->value_length()), i);
+        break;
+      case AttrType::BOOLEANS:
+        output_chunk_.add_column(make_unique<Column>(AttrType::BOOLEANS, sizeof(bool)), i);
+        break;
+      default:
+        ASSERT(false, "not supported value type");
+    }
+  }
+  for (size_t i = 0; i < aggregate_expressions_.size(); i++) {
+    auto &expr = aggregate_expressions_[i];
+    ASSERT(expr->type() == ExprType::AGGREGATION, "expected an aggregation expression");
+    auto *aggregate_expr = static_cast<AggregateExpr *>(expr);
+    switch (aggregate_expr->value_type()) {
+      case AttrType::INTS:
+        output_chunk_.add_column(make_unique<Column>(AttrType::INTS, sizeof(int)), i + group_size);
+        break;
+      case AttrType::FLOATS:
+        output_chunk_.add_column(make_unique<Column>(AttrType::FLOATS, sizeof(float)), i + group_size);
+        break;
+      default:
+        ASSERT(false, "not supported aggregation type");
+    }
+  }
+}
 
-  // PhysicalOperator &child = *children_[0];
-  // RC                rc    = child.open(trx);
-  // if (OB_FAIL(rc)) {
-  //   LOG_INFO("failed to open child operator. rc=%s", strrc(rc));
-  //   return rc;
-  // }
-  
-  // Chunk input_chunk;
-  // while (child.next(input_chunk) == RC::SUCCESS)
-  // {
-  //   for (size_t i = 0; i < input_chunk.rows(); i++)
-  //   {
-  //     auto col = input_chunk.column_ptr(i);
+RC GroupByVecPhysicalOperator::open(Trx* trx) {
+  ASSERT(children_.size() == 1, "group by operator only support one child, but got %d", children_.size());
 
-  //   }
-  // }
-  return RC::SUCCESS;
+  PhysicalOperator &child = *children_[0];
+  RC                rc    = child.open(trx);
+  if (OB_FAIL(rc)) {
+    LOG_INFO("failed to open child operator. rc=%s", strrc(rc));
+    return rc;
+  }
+  if (OB_SUCC(rc = child.next(chunk_))) {
+    Chunk group_chunk;
+    Chunk aggr_chunk;
+    for (auto i = 0;i < group_by_exprs_.size();i++) {
+      auto to_add = make_unique<Column>();
+      group_by_exprs_[i]->get_column(chunk_, *to_add);
+      group_chunk.add_column(std::move(to_add), i);
+    }
+    for (auto i = 0;i < aggregate_expressions_.size();i++) {
+      auto to_add = make_unique<Column>();
+      aggregate_expressions_[i]->get_column(chunk_, *to_add);
+      aggr_chunk.add_column(std::move(to_add), i);
+    }
+    hash_table_.add_chunk(group_chunk, aggr_chunk);
+  }
+  scanner_.open_scan();
+  if (rc == RC::RECORD_EOF) {
+    rc = RC::SUCCESS;
+  }
+  return rc;
+}
 
+RC GroupByVecPhysicalOperator::next(Chunk &chunk) {
+  RC rc = scanner_.next(output_chunk_);
+  if (rc == RC::RECORD_EOF) {
+    rc = RC::SUCCESS;
+  }
+  chunk.reference(output_chunk_);
+  return rc;
 }
